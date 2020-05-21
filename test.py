571c15e8781713dc1782cdfebde2b3c41d0d0761
baritone0211 <baritone.tut0211@gmail.com>
2020-05-21 11:12:24 +0900

    coding-test

--- test.py
+++ test.py
@@ -1,1 +1,26 @@
-print("Hello")
+
+
+def main():
+    print("coding-test")
+
+    with open("input.txt") as f:
+        lines = f.read()
+        lines = lines.split('\n')
+
+    m = int(lines[-2])
+    del lines[-2:]
+    lines2 = []
+
+    for l in lines:
+        line = l.split(":")
+        lines2.append(line)
+
+    for n in range(0, len(lines2)-1):
+        ni = int(lines2[n][0])
+        if m % ni == 0:
+            print(lines2[n][1])
+    
+    print(m)
+
+
+main()
